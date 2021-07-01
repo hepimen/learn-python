fd0938c70fcce989db9060b50f87d0956ec57858
hepimen <mhrzstudio@gmail.com>
2021-07-01 14:14:28 +0700

    Function

---
+++ function_arguments_2.py
@@ -0,0 +1,9 @@
+# Membuat Function dengan nama belajar_python yang memiliki dua Arguments: "name" dan "age"
+def belajar_python(name, age):
+    print("Halo, Saya %s. Umur saya %d" % (age, name))
+
+
+# Memanggil Function belajar_python
+# dengan Arguments "name" berupa string dengan value "Hepimen"
+# dan Arguments "age" berupa integer dengan value "30"
+belajar_python("Hepimen", 30)  # Halo, Saya Hepimen
---
+++ function_default_value_arguments.py
@@ -0,0 +1,7 @@
+# Membuat Function belajar_python dengan Arguments "name" dan menentukan Default valuenya sebagai "Hepimen".
+def belajar_python(name="Hepimen"):
+    print("Halo, Saya %s" % name)
+
+
+# Memanggil Function belajar_python tanpa mengirimkan Arguments nya.
+belajar_python()  # Halo, Saya Hepimen
